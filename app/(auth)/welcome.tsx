baa44266c5a1fffa8cbae812b909959e7a8a02a0
nzemia <mualukofrank@gmail.com>
2024-09-27 12:49:31 +0300

    welcome screens

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,82 @@
+import CustomButton from "@/components/CustomButton"
+import { onboarding } from "@/constants"
+import { useRouter } from "expo-router"
+import { useRef, useState } from "react"
+import { Image, Text, TouchableOpacity, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+import Swiper from "react-native-swiper"
+
+const WelcomeOnboarding = () => {
+    const router = useRouter()
+    const swiperRef = useRef<Swiper>(null)
+    const [activeIndex, setActiveIndex] = useState(0)
+
+    const isLandSlide = activeIndex === onboarding.length - 1
+
+    return (
+        <SafeAreaView className="flex h-full items-center justify-between bg-white">
+            <TouchableOpacity
+                className="w-full flex justify-end items-end p-5"
+                onPress={() => {
+                    router.replace("/(auth)/sign-up")
+                }}
+            >
+                <Text className="text-black text-md font-JakartaBold">
+                    Skip
+                </Text>
+            </TouchableOpacity>
+
+            {/**Swiping left and right pages. used npm i react-native-swiper */}
+            <Swiper
+                ref={swiperRef}
+                loop={false}
+                dot={
+                    <View className="w-[50px] h-[4px] mx-1 bg-[#afb8c4] rounded-full" />
+                }
+                activeDot={
+                    <View className="w-[50px] h-[8px] mx-1 bg-[#0286FF] rounded-full" />
+                }
+                onIndexChanged={index => setActiveIndex(index)}
+            >
+                {onboarding.map(item => (
+                    <View
+                        key={item.id}
+                        className="flex items-center justify-center p-5"
+                    >
+                        <Image
+                            source={item.image}
+                            className="w-full h-[300px] "
+                            resizeMode="contain"
+                        />
+                        {/**Text */}
+                        <View className="flex flex-row mt-10 items-center justify-center w-full">
+                            <Text className="text-black text-3xl font-bold mx-10">
+                                {item.title}
+                            </Text>
+                        </View>
+
+                        {/**Description */}
+                        <Text
+                            className="text-md font-JakartaSemiBold text-center text-[#858585] 
+                                mx-10 mt-3"
+                        >
+                            {item.description}
+                        </Text>
+                    </View>
+                ))}
+            </Swiper>
+
+            <CustomButton
+                title={isLandSlide ? "Get Started" : "Next"}
+                onPress={() =>
+                    isLandSlide
+                        ? router.replace("/(auth)/sign-up")
+                        : swiperRef.current?.scrollBy(1)
+                }
+                className="w-11/12  mb-5"
+            />
+        </SafeAreaView>
+    )
+}
+
+export default WelcomeOnboarding
