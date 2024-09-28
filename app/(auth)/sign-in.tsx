eda6b60508f47c7b5826cddbe66025c57f8ebb25
nzemia <mualukofrank@gmail.com>
2024-09-28 15:23:19 +0300

    authentication and bottom navigation

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,52 +1,75 @@
+import { useSignIn } from "@clerk/clerk-expo"
+import { Link, router } from "expo-router"
+import { useCallback, useState } from "react"
+import { Alert, Image, ScrollView, Text, View } from "react-native"
+
 import CustomButton from "@/components/CustomButton"
 import InputField from "@/components/InputField"
 import OAuth from "@/components/OAuth"
 import { icons, images } from "@/constants"
-import { Link } from "expo-router"
-import { useState } from "react"
-import { Image, ScrollView, Text, View } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
 
 const SignIn = () => {
-    const [form, setForm] = useState({        
+    const { signIn, setActive, isLoaded } = useSignIn()
+
+    const [form, setForm] = useState({
         email: "",
         password: ""
     })
 
-    const onSignInPress = async () => {}
+    const onSignInPress = useCallback(async () => {
+        if (!isLoaded) return
+
+        try {
+            const signInAttempt = await signIn.create({
+                identifier: form.email,
+                password: form.password
+            })
+
+            if (signInAttempt.status === "complete") {
+                await setActive({ session: signInAttempt.createdSessionId })
+                router.replace("/(root)/(tabs)/home")
+            } else {
+                // See https://clerk.com/docs/custom-flows/error-handling for more info on error handling
+                console.log(JSON.stringify(signInAttempt, null, 2))
+                Alert.alert("Error", "Log in failed. Please try again.")
+            }
+        } catch (err: any) {
+            console.log(JSON.stringify(err, null, 2))
+            Alert.alert("Error", err.errors[0].longMessage)
+        }
+    }, [isLoaded, form])
+
     return (
-        <SafeAreaView className="flex-1 bg-white">
-            <ScrollView className="flex-1 bg-white">
-                <View className="flex-1 bg-white">
-                    <View className="relative w-full h-[250px]">
-                        <Image
-                            source={images.signUpCar}
-                            className="z-0 w-full h-[250px]"
-                        />
-                        <Text
-                            className="text-2xl text-black font-JakartaSemiBold absolute 
-                            bottom-5 left-5"
-                        >
-                            Welcome Back 👋🏿
-                        </Text>
-                    </View>
+        <ScrollView className="flex-1 bg-white">
+            <View className="flex-1 bg-white">
+                <View className="relative w-full h-[250px]">
+                    <Image
+                        source={images.signUpCar}
+                        className="z-0 w-full h-[250px]"
+                    />
+                    <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+                        Welcome 👋
+                    </Text>
                 </View>
 
                 <View className="p-5">
                     <InputField
                         label="Email"
-                        placeholder="Enter your email"
+                        placeholder="Enter email"
                         icon={icons.email}
+                        textContentType="emailAddress"
                         value={form.email}
                         onChangeText={value =>
                             setForm({ ...form, email: value })
                         }
                     />
+
                     <InputField
                         label="Password"
-                        placeholder="Enter your password"
-                        secureTextEntry={true}
+                        placeholder="Enter password"
                         icon={icons.lock}
+                        secureTextEntry={true}
+                        textContentType="password"
                         value={form.password}
                         onChangeText={value =>
                             setForm({ ...form, password: value })
@@ -54,26 +77,23 @@ const SignIn = () => {
                     />
 
                     <CustomButton
-                        title="Login"
-                        className="mt-10"
+                        title="Sign In"
                         onPress={onSignInPress}
+                        className="mt-6"
                     />
 
-                    {/**OAuth  */}
                     <OAuth />
 
                     <Link
                         href="/sign-up"
                         className="text-lg text-center text-general-200 mt-10"
                     >
-                        <Text>Don't have an account? {"  "}</Text>
+                        Don't have an account?{" "}
                         <Text className="text-primary-500">Sign Up</Text>
                     </Link>
                 </View>
-
-                {/**Modal verification */}
-            </ScrollView>
-        </SafeAreaView>
+            </View>
+        </ScrollView>
     )
 }
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,25 @@
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo"
 import { useFonts } from "expo-font"
 import { Stack } from "expo-router"
 import * as SplashScreen from "expo-splash-screen"
 import { useEffect } from "react"
 import "react-native-reanimated"
+import { LogBox } from "react-native"
+
+import { tokenCache } from "@/lib/auth"
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+    throw new Error(
+        "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+    )
+}
+
+LogBox.ignoreLogs(["Clerk:"])
 
 export default function RootLayout() {
     const [loaded] = useFonts({
@@ -17,6 +31,7 @@ export default function RootLayout() {
         Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
         "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf")
     })
+
     useEffect(() => {
         if (loaded) {
             SplashScreen.hideAsync()
@@ -28,11 +43,24 @@ export default function RootLayout() {
     }
 
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="(root)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-        </Stack>
+        <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+            <ClerkLoaded>
+                <Stack>
+                    <Stack.Screen
+                        name="index"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="(auth)"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="(root)"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen name="+not-found" />
+                </Stack>
+            </ClerkLoaded>
+        </ClerkProvider>
     )
 }
