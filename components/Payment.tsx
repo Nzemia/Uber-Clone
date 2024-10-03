35abc9674ce0a9148989f0fe24b8f66f457a60e1
nzemia <mualukofrank@gmail.com>
2024-10-03 20:30:49 +0300

    google OAuth

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -45,7 +45,7 @@ const Payment = ({
 
     const initializePaymentSheet = async () => {
         const { error } = await initPaymentSheet({
-            merchantDisplayName: "Example, Inc.",
+            merchantDisplayName: "Uber Clone.",
             intentConfiguration: {
                 mode: {
                     amount: parseInt(amount) * 100,
